bdcafc1de415a56448c56e8cbc2b99d0164fba96
UzairRamzan06 <mramzandxb06@gmail.com>
2026-06-28 17:17:50 +0400

    Add mini_project_calculator.py for Week-01
    Initial commit for Week-01 mini project calculator.

---
+++ Week-01/mini_project_calculator.py
@@ -0,0 +1,12 @@
+# This is your first Week-01 project.
+# It combines everything you learned.
+
+# It should include:
+Variables
+Input
+Operators
+if-else
+Functions
+Loops
+
+# This project demonstrates that you can apply multiple concepts together.
