d966951fa05234bd4febe5de59a2bb4ba28a3bdb
UzairRamzan06 <mramzandxb06@gmail.com>
2026-06-28 17:00:43 +0400

    Create 06_operators.py for operator exercises
    Added a new Python file for operators practice.

---
+++ Week-01/06_operators.py
@@ -0,0 +1,14 @@
+# Course : Artificial Intelligence with Python
+# Week   : 01
+# File   : 06_operators.py
+# VDO Lectures Covered : 15
+# Topic  : Python Operators - Arithmetic, Comparison, Logical & Assignment
+# What to practice
+    Arithmetic
+    Comparison
+    Logical
+    Assignment
+# ===========================================
+
+
+   
