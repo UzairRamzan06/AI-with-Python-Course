86bb1e4f48d7dafcf304fd93eff2204d43192dda
UzairRamzan06 <mramzandxb06@gmail.com>
2026-06-28 16:57:53 +0400

    Document type conversion and casting examples
    Added comments and examples for type casting in Python.

--- Week-01/05_type_casting.py
+++ Week-01/05_type_casting.py
@@ -1,1 +1,32 @@
+# Course : Artificial Intelligence with Python
+# Week   : 01
+# File   : 05_type_casting.py
+# VDO Lectures Covered : 14
+# Topic  : Type Conversion & Casting in Python 
+# What to practice
+    int()
+    float()
+    str()
+    bool()
+# ===========================================
+
+age = "25"
+age = int(age)
+print(age)
+
+# ===========================================
+
+
+age = 25
+score = 3.6
+name = "Ali Zafar"
+is_passed = True
+
+contains these data types:
+| Variable    | Value         | Data Type        |
+| ----------- | ------------- | ---------------- |
+| `age`       | `25`          | Integer (`int`)  |
+| `score`     | `3.6`         | Float (`float`)  |
+| `name`      | `"Ali Zafar"` | String (`str`)   |
+| `is_passed` | `True`        | Boolean (`bool`) |
 
