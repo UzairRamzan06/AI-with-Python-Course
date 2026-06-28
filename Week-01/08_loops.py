bdb98d9b6f8c4c5b5b1504fccf71d30bb9613829
UzairRamzan06 <mramzandxb06@gmail.com>
2026-06-28 17:08:32 +0400

    Update lecture topics and practice sections in 08_loops.py

---
+++ Week-01/08_loops.py
@@ -0,0 +1,20 @@
+# Course : Artificial Intelligence with Python
+# Week   : 01
+# File   : 08_loops.py
+# VDO Lectures Covered : 18 , 19 , 20 , 21 , 22 
+# Topic  : FOR Loop , WHILE Loop , Nested Loops , Looping through Strings , Looping through Dictionaries.
+
+          lecture vdo # 18 > FOR Loop
+          lecture vdo # 19 > WHILE Loop
+          lecture vdo # 20 > Nested Loops
+          lecture vdo # 21 > Looping through Strings
+          lecture vdo # 22 > Looping through Dictionaries
+
+# What to practice
+    for
+    while
+    nested
+    string loop
+    dictionary loop
+
+# ===========================================
