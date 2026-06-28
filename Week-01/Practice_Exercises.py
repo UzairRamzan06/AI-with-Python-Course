9b6ac87d0a50cca0b02be3e9a4523d44989737d9
UzairRamzan06 <mramzandxb06@gmail.com>
2026-06-28 17:16:01 +0400

    Add Practice Exercises to Week-01

---
+++ Week-01/Practice_Exercises.py
@@ -0,0 +1,23 @@
+Exercise 1
+Simple Calculator
+
+Exercise 2
+Odd Even Checker
+
+Exercise 3
+Voting Eligibility
+
+Exercise 4
+Multiplication Table
+
+Exercise 5
+Grade Calculator
+
+Exercise 6
+Factorial
+
+Exercise 7
+Dictionary Practice
+
+Exercise 8
+Functions Practice
