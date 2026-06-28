c22b7e1aad277d297d1e5d8b5c5d55497cf9e36c
UzairRamzan06 <mramzandxb06@gmail.com>
2026-06-28 17:03:18 +0400

    Create 07_conditionals.py

---
+++ Week-01/07_conditionals.py
@@ -0,0 +1,20 @@
+# Course : Artificial Intelligence with Python
+# Week   : 01
+# File   : 07_conditionals.py
+# VDO Lectures Covered : 16 & 17
+# Topic  : Conditional Statement - Part I  & Conditional Statement - Part II 
+# What to practice
+
+
+
+# Also
+    Grade calculator
+    Even/Odd
+    Positive/Negative
+# ===========================================
+
+age = 18
+if age >= 18:
+    print("Eligible")
+else:
+    print("Not Eligible")
