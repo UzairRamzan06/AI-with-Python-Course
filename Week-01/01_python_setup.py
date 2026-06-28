835a0ccc45d044bbe03dae5bc58a7ee3c45d537e
UzairRamzan06 <mramzandxb06@gmail.com>
2026-06-28 14:34:08 +0400

    Update 01_python_setup.py

--- Week-01/01_python_setup.py
+++ Week-01/01_python_setup.py
@@ -1,1 +1,19 @@
+# ===========================================
+# Course : Artificial Intelligence with Python
+# Week   : 01
+# File   : 01_python_setup.py
+# Lectures Covered : 09 - 10
+# Topic  : Python Installation & First Program
+# ===========================================
 
+# Display a welcome message
+print("Hello, World!")
+
+# Display course name
+print("Artificial Intelligence with Python")
+
+# Display learner information
+print("Learning Python Programming")
+
+# First Python practice
+print("Python is easy to learn.")
