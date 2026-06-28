bba66dec69ca1ed16ccf8ddf0be1cb52829f7998
UzairRamzan06 <mramzandxb06@gmail.com>
2026-06-28 17:09:39 +0400

    Update 08_loops.py

--- Week-01/08_loops.py
+++ Week-01/08_loops.py
@@ -4,11 +4,11 @@
 # VDO Lectures Covered : 18 , 19 , 20 , 21 , 22 
 # Topic  : FOR Loop , WHILE Loop , Nested Loops , Looping through Strings , Looping through Dictionaries.
 
-          lecture vdo # 18 > FOR Loop
-          lecture vdo # 19 > WHILE Loop
-          lecture vdo # 20 > Nested Loops
-          lecture vdo # 21 > Looping through Strings
-          lecture vdo # 22 > Looping through Dictionaries
+          lecture vdo # 18 > FOR Loop in Python 
+          lecture vdo # 19 > WHILE Loop in Python 
+          lecture vdo # 20 > Nested Loops in Python 
+          lecture vdo # 21 > Looping Over Data Structures - Strings 
+          lecture vdo # 22 > Looping Over Data Structures - Dictionaries
 
 # What to practice
     for
