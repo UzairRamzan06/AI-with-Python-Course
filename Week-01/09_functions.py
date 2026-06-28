4abbae39391cf2364d7e6339247488b45f9cadf7
UzairRamzan06 <mramzandxb06@gmail.com>
2026-06-28 17:14:40 +0400

    Create 09_functions.py

---
+++ Week-01/09_functions.py
@@ -0,0 +1,21 @@
+# Course : Artificial Intelligence with Python
+# Week   : 01
+# File   : 09_functions.py
+# VDO Lectures Covered : 23 , 24 , 25 , 26
+# Topic  : Functions ,  Parameters , Return Values , Variable Scope , Lambda Functions
+ 
+          lecture vdo # 23 > Defining & Calling Functions 
+          lecture vdo # 24 > Function Parameters & Return Values
+          lecture vdo # 25 > Variable Scope in Python - Local vs Global 
+          lecture vdo # 26 > Lambda Functions in Python 
+
+
+# What to practice
+      Creating functions
+      Parameters
+      Return
+      Local variable
+      Global variable
+      Lambda
+  
+# ===========================================
