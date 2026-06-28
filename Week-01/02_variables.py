0e7d3ccc2014d73e4e06a6467d48f941eb95f292
UzairRamzan06 <mramzandxb06@gmail.com>
2026-06-28 14:40:05 +0400

    Create 02_variables.py
    VDO Lecture 11 
    Topic : Variables & Naming Conventions
    
    What to practice : 
    Variable creation
    Naming rules
    Multiple assignment
    Printing variables

---
+++ Week-01/02_variables.py
@@ -0,0 +1,21 @@
+# ===========================================
+# Course : Artificial Intelligence with Python
+# Week   : 01
+# File   : 02_variables.py
+# VDO Lectures Covered : 11
+# Topic  : Variables & Naming Conventions
+# What to practice
+    Variable creation
+    Naming rules
+    Multiple assignment
+    Printing variables
+# ===========================================
+
+
+name = "Ali"
+age = 22
+country = "Pakistan"
+
+print(name)
+print(age)
+print(country)
