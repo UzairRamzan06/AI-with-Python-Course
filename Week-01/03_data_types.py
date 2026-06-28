e9176a54bdb3821bc1f0280aafc16b4457cfe68d
UzairRamzan06 <mramzandxb06@gmail.com>
2026-06-28 15:32:29 +0400

    Add data types examples to 03_data_types.py
    Added data types examples including age, price, name, and is_student.

---
+++ Week-01/03_data_types.py
@@ -0,0 +1,21 @@
+# ===========================================
+# Course : Artificial Intelligence with Python
+# Week   : 01
+# File   : 03_data_types.py
+# VDO Lectures Covered : 12
+# Topic  : Numbers, Strings, Booleans
+# What to practice
+
+
+
+# ===========================================
+
+age = 22
+price = 199.99
+name = "Ali"
+is_student = True
+
+print(type(age))
+print(type(price))
+print(type(name))
+print(type(is_student))
